76eb94e8164380eb2916ec93e7be21ea3a9b54dc
epifantsevsa <epifantsev.sa@gmail.com>
2024-09-24 09:49:25 +0300

    Added pytest for testdata

---
+++ tests/test_check.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pytest
+import filecmp
+import os
+
+def iterate_through_files(ref_path, test_path, relative_path):
+    for file in os.listdir(ref_path):
+        ref_file_path = os.path.join(ref_path, file)
+        if os.path.isfile(ref_file_path):
+            test_file_path = os.path.join(test_path, relative_path, file)
+
+            if file == "amplitudes.csv":
+                test_df = pd.read_csv(test_file_path)
+                ref_df = pd.read_csv(ref_file_path)
+
+                assert test_df.iloc[:, :2].equals(ref_df.iloc[:, :2])
+            else:
+                assert filecmp.cmp(test_file_path, ref_file_path, shallow=False)
+        else:
+            iterate_through_files(os.path.join(ref_path, file), test_path, os.path.join(relative_path, file))
+
+def test_check_fly():
+    os.system("optimalTAD check")
+
+    ref_path = "/tank/projects/esa_scHiC/Tan2021/optimalTAD/tests/reference_files/"
+    test_path = "/tank/projects/esa_scHiC/Tan2021/optimalTAD/testouput/"
+    relative_path = ""
+
+    iterate_through_files(ref_path, test_path, relative_path)
